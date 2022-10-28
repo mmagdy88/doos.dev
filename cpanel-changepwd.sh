a69b1a4493c6e429d482643f827f0134561a0d63
mmagdy88 <s3cur1ty1@hotmail.com>
2022-10-28 06:43:38 +0200

    Updated script

--- cpanel-changepwd.sh
+++ cpanel-changepwd.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
-# Coded by NixCP, Modified by Mohamed Magdy
+# Developed by Mohamed Magdy
+
+# Cleaning
+rm -f /root/user-pass.txt
 
 # Install expect
 yum install expect -y
 
-# Avoid cPanel warnings
-ALLOW_PASSWORD_CHANGE=1
-export ALLOW_PASSWORD_CHANGE=1
-
 # List all users and set random strong passwords
-ls -1 /var/cpanel/users | while read user; do
-pass=`mkpasswd -l 16 -s 0`
-echo "$user $pass" >> user-pass.txt
+ls -1 /var/cpanel/users | grep -v system | while read user; do
+pass=`mkpasswd -l 18 -s 3`
+echo "$user $pass" >> /root/user-pass.txt
 
 # Change the password & update FTP login database
-/scripts/ftpupdate
-/scripts/realchpass $user $pass
-
+whmapi1 passwd user="$user" password="$pass"
 done
+
+# Info
+echo "Passwords are located in /root/user-pass.txt"
